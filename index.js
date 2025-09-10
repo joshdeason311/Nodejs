4064b006b8fc46b385e1a97b9c88446cb3f6564e
jdeason <joshdeason@gmail.com>
2025-09-09 20:55:48 -0600

    Updated greeting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Node.js!'));
+app.get('/', (req, res) => res.send('Hello from Node.js on Azure!'));
 app.listen(process.env.PORT || 3000, () => {
 console.log('Server is running...');
 });
