206eb1851adb43afdc3135643afb52f8a0b1d243
jdeason <joshdeason@gmail.com>
2025-09-09 21:01:47 -0600

    Resolved merge conflicts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Node.js on Azure!'));
+app.get('/', (req, res) => res.send('Hello from Node.js on Azure! Have a good day!'));
 app.listen(process.env.PORT || 3000, () => {
 console.log('Server is running...');
-});
+});git add .
